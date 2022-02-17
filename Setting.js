b3272fb8d6df430aabec126d073c08c331c073fe
Aiden-Patterson <pat18001@byui.edu>
2022-02-17 16:14:11 -0700

    added login screen

--- Setting.js
+++ Setting.js
@@ -2,6 +2,7 @@ import React from "react";
 import { StyleSheet, Text, View, Image } from 'react-native';
 import { Card, ListItem, Button, Icon } from 'react-native-elements';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import Login from './Login.js'
 
 
 function Setting() {
@@ -9,9 +10,10 @@ function Setting() {
       <View>
       <Button
       title="Log In"
-      onPress={() => Alert.alert('Simple Button pressed')}
+      onPress={() => {return<Login></Login>}}
     />
       </View>
+      
     )
   }
 
