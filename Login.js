1fc876bc799fcce09f9de17ccd7bfdbbcc636335
Aiden-Patterson <pat18001@byui.edu>
2022-03-25 16:54:42 -0500

    added password verification

--- Login.js
+++ Login.js
@@ -1,15 +1,14 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { SafeAreaView, StyleSheet, TextInput } from "react-native";
 import { View, Text, Button } from "react-native";
 
 //const [phoneNumber, setPhoneNumber] = useState("");
 //const [oneTimePassword, setOneTimePassword] = useState("");
 
-
 export default function Login(props) {
-const [phoneNumber, setPhoneNumber] = useState(phoneNumber);
-const [oneTimePassword, setOneTimePassword] = useState("");
-const [password, setPassword] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState(phoneNumber);
+  const [oneTimePassword, setOneTimePassword] = useState("");
+  const [password, setPassword] = useState("");
   return (
     <View>
       <Text style={styles.login}>Log in to continue</Text>
@@ -24,15 +23,15 @@ const [password, setPassword] = useState("");
         style={styles.input}
         title="Generate Password"
         onPress={() => {
-          fetch("https://dev.stedi.me/oneTimePassword/"+phoneNumber,{
-            type: "POST",
-            headers:{
-              Accept:"application/text",
-              "Content-Type":"application/text"
-            }
-            
+          fetch("https://dev.stedi.me/twofactorlogin/" + phoneNumber, {
+            method: "POST",
+            headers: {
+              Accept: "application/text",
+              "Content-Type": "application/text",
+            },
           });
-        }} 
+          
+        }}
       ></Button>
       <TextInput
         style={styles.input}
@@ -45,8 +44,8 @@ const [password, setPassword] = useState("");
         style={styles.input}
         title="Log In"
         onPress={() => {
-          props.setUserLoggedIn(true)}
-      }
+          if (oneTimePassword == password && password > 0) props.setUserLoggedIn(true);
+        }}
       ></Button>
     </View>
   );
@@ -66,7 +65,6 @@ const styles = StyleSheet.create({
     fontSize: 30,
   },
   input: {
-    //flexDirection: "column",
     height: "15%",
     fontSize: 23,
   },
